1965c3226354caae4ffe296afd84d0d1905e93dc
Aditya <aditya09377@gmail.com>
2023-02-27 15:33:06 +0530

    my first commit

---
+++ cart.js
@@ -0,0 +1,18 @@
+
+let ytgj = [{
+    name: "Volvo",
+    price: 24
+}, {
+    name: "Saab",
+    price: 400
+}, {
+    name: "Mercedes",
+    price: 500
+}, {
+    name: "Audi",
+    price: 95
+}
+];
+let item = document.getElementById("cars")
+
+// console.log(item.value);            
